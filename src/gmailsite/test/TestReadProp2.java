8ebf5d5515e5001af416afc79648518be7a229f9
binh.nhu <thanhbinh0990@gmail.com>
2016-12-01 21:42:30 +0700

    test builder

--- src/gmailsite/test/TestReadProp2.java
+++ src/gmailsite/test/TestReadProp2.java
@@ -3,19 +3,38 @@ package gmailsite.test;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import gmailsite.action.WebDriverAction;
+import gmailsite.object.Account;
 
 public class TestReadProp2 {
 
 	public static void main(String[] args) {
 		
-		FirefoxDriver driver = new FirefoxDriver();
-		WebDriverAction action = new WebDriverAction(driver);
+//		FirefoxDriver driver = new FirefoxDriver();
+//		WebDriverAction action = new WebDriverAction(driver);
+//		
+//		String a = action.getPropertiesFileValue("StorefrontUrl");
+//		
+//		System.out.println(a);
 		
-		String a = action.getPropertiesFileValue("StorefrontUrl");
+		Account account = new Account.Builder()
+				.username("Binh Nhu")
+				.password("abc")
+				.staySignedInOption("Yes")
+				.build();
 		
-		System.out.println(a);
-		
+		System.out.println(account.getUsername());
+		System.out.println(account.getPassword());
+		System.out.println(account.getStaySignedInOption());
 
+		Account account1 = new Account.Builder()
+				.username("Binh Nhu")
+				.password("abc")
+				//.staySignedInOption("Yes")
+				.build();
+		
+		System.out.println(account1.getUsername());
+		System.out.println(account1.getPassword());
+		System.out.println(account1.getStaySignedInOption());
 	}
 
 }
